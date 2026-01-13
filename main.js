c2bf78d4a2c40171adb8508d8ec273c075f3b4bf
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-01-13 14:34:30 +0100

    feat(L2): corrigé

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
+            product: 'Gauss',
             // Solution
-            description: 'A warm fuzzy pair of socks.' 
+            description: 'la vitesse incarné' 
             // Solution
         }
     }
